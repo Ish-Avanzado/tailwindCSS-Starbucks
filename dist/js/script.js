7c5a1ef2b193590e9b2cf32fbf368159a385485a
bcbhhx <christianjadeflores47@gmail.com>
2025-04-08 15:57:31 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+//burger
+const burger = document.querySelector(".burger__menu");
+const headerA = document.querySelector(".header__action");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("open");
+  headerA.classList.toggle("open");
+});
+
+//links
+const links = document.querySelectorAll(".links__item h4");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.querySelector("i").classList.toggle("open");
+    link.nextElementSibling.classList.toggle("open");
+  });
+});
